0352c5de9eef600c44b4273ff35ef9aaefd287e8
Martin Herrmann <martin.herrmann@tum.de>
2014-10-19 13:36:11 +0200

    mher: Added more comments, formatted for upload, created a tarball.

--- assignment01/assignment01_mod.c
+++ assignment01/assignment01_mod.c
@@ -1,8 +1,14 @@
+/*
+ * Assignment 01 for the course Rootkit Programming at TUM in WS2014/15.
+ * Implemented by Guru Chandrasekhara and Martin Herrmann
+ */
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include "sysmap.h"
 
-// create the function nr_processes() to be used later 
+/*
+ * create the function nr_processes() to be used later 
+ */
 typedef int func(void);
 func* nr_processes = (func*) sysmap_nr_processes;
 
@@ -20,6 +26,10 @@ int print_nr_procs(void)
 	return 0;
 }
 
+/*
+ * Function called when loading the kernel module.
+ * Prints a welcome-message and calls the print_nr_procs() function.
+ */
 int init_module(void)
 {
 	printk(KERN_INFO "Welcome! Loading kernel module assignment01.\n");
@@ -29,6 +39,10 @@ int init_module(void)
 	return 0;
 }
 
+/*
+ * Function called when unloading the kernel module.
+ * Prints a goodbye-message.
+ */
 void cleanup_module(void)
 {
 	printk(KERN_INFO "Unloading kernel module assignment01. Bye!\n");
