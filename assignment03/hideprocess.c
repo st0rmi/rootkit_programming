a0750861410ac4f3330adcb226057f31109cb6ff
Martin Herrmann <martin.herrmann@tum.de>
2014-10-29 14:04:01 +0100

    mher: Added a macro from an old kernel version to test if it still works.

--- assignment03/hideprocess.c
+++ assignment03/hideprocess.c
@@ -12,7 +12,18 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Guru Chandrasekhara, Martin Herrmann");
 
-
+#define REMOVE_LINKS(p) do { unsigned long flags; \
+	save_flags(flags) ; cli(); \
+	(p)->next_task->prev_task = (p)->prev_task; \
+	(p)->prev_task->next_task = (p)->next_task; \
+	restore_flags(flags); \
+	if ((p)->p_osptr) \
+		(p)->p_osptr->p_ysptr = (p)->p_ysptr; \
+	if ((p)->p_ysptr) \
+		(p)->p_ysptr->p_osptr = (p)->p_osptr; \
+	else \
+		(p)->p_pptr->p_cptr = (p)->p_osptr; \
+	} while (0)
 
 
 static int task_count = 0;
