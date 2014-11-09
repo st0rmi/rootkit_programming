3c248d2c40e394f48287fffaba94e04b96a59b09
Martin Herrmann <martin.herrmann@tum.de>
2014-11-09 17:13:44 +0100

    mher: First round of cleanup

--- assignment04/.hidefile.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/martin/rootkit_programming/assignment04/hidefile.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/martin/rootkit_programming/assignment04/hidefile.ko /home/martin/rootkit_programming/assignment04/hidefile.o /home/martin/rootkit_programming/assignment04/hidefile.mod.o
--- assignment04/.tmp_versions/hidefile.mod
+++
@@ -1,2 +0,0 @@
-/home/martin/rootkit_programming/assignment04/hidefile.ko
-/home/martin/rootkit_programming/assignment04/hidefile.o
