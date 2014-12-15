dcd104751c8c0bc13ec99c68ec715384352a5500
Martin Herrmann <martin.herrmann@tum.de>
2014-12-15 14:51:09 +0100

    mher: Removed unnecessary files.

--- new_structure/getdents.h
+++
@@ -1,9 +0,0 @@
-#ifndef GETDENTS_HEADER
-#define GETDENTS_HEADER
-
-#include "include.h"
-
-void hook_getdents(void);
-void unhook_getdents(void);
-
-#endif
--- new_structure/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_HEADER
-#define MAIN_HEADER
-
-/* Information for modinfo */
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Guru Chandrasekhara, Martin Herrmann");
-MODULE_DESCRIPTION("A rootkit for the course Rootkit Programming at TUM in WS2014/25");
-
-#endif
